1c6cbdc72a6232c61880ad4acf8853aa0e8641fe
SEAC-Yichi <Yichi.Zhang@Unilever.com>
2022-07-14 02:03:05 +0100

    corrected programs

--- Percolation.java
+++ Percolation.java
@@ -1,171 +1,169 @@
+import edu.princeton.cs.algs4.StdOut;
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 public class Percolation {
-    private int[][] open;
-    private int[][] ind;
+    private int size;
+    private int nOpen;
+    private int number;
+    private boolean[][] open;
     private WeightedQuickUnionUF uf;
+
 
     // creates n-by-n grid, with all sites initially blocked
     public Percolation(int n) {
+        nOpen = 0;
+        size = n;
+        number = n * n;
         if (n <= 0) {
             throw new IllegalArgumentException("n must be positive");
         }
-        open = new int[n][n];
-        ind = new int[n][n];
-        for (int row = 0; row < n; row++) {
-            for (int col = 0; col < n; col++) {
-                ind[row][col] = row * n + col;
-            }
-        }
-        uf = new WeightedQuickUnionUF(n * n + 2);
+        open = new boolean[n][n];
+        uf = new WeightedQuickUnionUF(number + 2);
     }
 
     // opens the site (row, col) if it is not open already
     public void open(int row, int col) {
-        int n = open.length;
-        if (!(row > 0 && row <= open.length && col > 0 && col <= open.length)) {
+        if (row <= 0 || row > size || col <= 0 || col > size) {
             throw new IllegalArgumentException("row, col indices between 1 and n");
         }
-        open[row - 1][col - 1] = 1;
+        open[row - 1][col - 1] = true;
+        nOpen++;
         if (row == 1) {
+            uf.union(0, col);
             if (col == 1) {
                 if (isOpen(1, 2))
-                    uf.union(ind[0][0], ind[0][1]);
+                    uf.union(1, 2);
                 if (isOpen(2, 1))
-                    uf.union(ind[0][0], ind[1][0]);
+                    uf.union(1, size + 1);
                 return;
             }
-            if (col == n) {
-                if (isOpen(1, n - 1))
-                    uf.union(ind[0][n - 1], ind[0][n - 2]);
-                if (isOpen(2, n))
-                    uf.union(ind[0][n - 1], ind[1][n - 1]);
+            if (col == size) {
+                if (isOpen(1, size - 1))
+                    uf.union(size, size - 1);
+                if (isOpen(2, size))
+                    uf.union(size, 2 * size);
                 return;
             }
-            else {
-                if (isOpen(1, col - 1))
-                    uf.union(ind[0][col - 1], ind[0][col - 2]);
-                if (isOpen(1, col + 1))
-                    uf.union(ind[0][col - 1], ind[0][col]);
-                if (isOpen(2, col))
-                    uf.union(ind[0][col - 1], ind[1][col - 1]);
-                return;
-            }
+
+            if (isOpen(1, col - 1))
+                uf.union(col, col - 1);
+            if (isOpen(1, col + 1))
+                uf.union(col, col + 1);
+            if (isOpen(2, col))
+                uf.union(col, size + col);
+            return;
         }
 
-        if (row == n) {
+
+        if (row == size) {
+            uf.union(number + 1, number - size + col);
             if (col == 1) {
-                if (isOpen(n, 2))
-                    uf.union(ind[n - 1][0], ind[n - 1][1]);
-                if (isOpen(n - 1, 1))
-                    uf.union(ind[n - 1][0], ind[n - 2][0]);
+                if (isOpen(size, 2))
+                    uf.union(number - 2 * size, number - size + 2);
+                if (isOpen(size - 1, 1))
+                    uf.union(number - 2 * size, number - 2 * size + 1);
                 return;
             }
 
-            if (col == n) {
-                if (isOpen(n, n - 1))
-                    uf.union(ind[n - 1][n - 1], ind[n - 1][n - 2]);
-                if (isOpen(n - 1, n))
-                    uf.union(ind[n - 1][n - 1], ind[n - 2][n - 1]);
+            if (col == size) {
+                if (isOpen(size, size - 1))
+                    uf.union(number, number - 1);
+                if (isOpen(size - 1, size))
+                    uf.union(number, number - size);
                 return;
             }
-            else {
-                if (isOpen(n, col - 1))
-                    uf.union(ind[n - 1][col - 1], ind[n - 1][col - 2]);
-                if (isOpen(n, col + 1))
-                    uf.union(ind[n - 1][col - 1], ind[n - 1][col]);
-                if (isOpen(n - 1, col))
-                    uf.union(ind[n - 1][col - 1], ind[n - 2][col - 1]);
-                return;
 
-            }
+            if (isOpen(size, col - 1))
+                uf.union(number - size + col, number - size + col - 1);
+            if (isOpen(size, col + 1))
+                uf.union(number - size + col, number - 2 * size + col);
+            if (isOpen(size - 1, col))
+                uf.union(number - size + col, number - size + col);
+            return;
+
+
         }
         if (col == 1) {
             if (isOpen(row, 2))
-                uf.union(ind[row - 1][0], ind[row - 1][1]);
+                uf.union((row - 1) * size + 1, (row - 1) * size + 2);
             if (isOpen(row + 1, 1))
-                uf.union(ind[row - 1][0], ind[row][0]);
+                uf.union((row - 1) * size + 1, row * size + 1);
             if (isOpen(row - 1, 1))
-                uf.union(ind[row - 1][0], ind[row - 2][0]);
+                uf.union((row - 1) * size + 1, (row - 2) * size + 1);
             return;
 
         }
-        if (col == n) {
-            if (isOpen(row - 1, n))
-                uf.union(ind[row - 1][n - 1], ind[row - 2][n - 1]);
-            if (isOpen(row + 1, n))
-                uf.union(ind[row - 1][n - 1], ind[row][n - 1]);
-            if (isOpen(row, n - 1))
-                uf.union(ind[row - 1][n - 1], ind[row - 1][n - 2]);
+        if (col == size) {
+            if (isOpen(row - 1, size))
+                uf.union(row * size, (row - 1) * size);
+            if (isOpen(row + 1, size))
+                uf.union(row * size, (row + 1) * size);
+            if (isOpen(row, size - 1))
+                uf.union(row * size, row * size - 1);
             return;
         }
 
-        if (isOpen(row, col + 1))
-            uf.union(ind[row - 1][col - 1], ind[row - 1][col]);
-        if (isOpen(row, col - 1))
-            uf.union(ind[row - 1][col - 1], ind[row - 1][col - 2]);
-        if (isOpen(row - 1, col))
-            uf.union(ind[row - 1][col - 1], ind[row - 2][col - 1]);
-        if (isOpen(row + 1, col))
-            uf.union(ind[row - 1][col - 1], ind[row][col - 1]);
+        if (
+
+                isOpen(row, col + 1))
+            uf.union((row - 1) * size + col, (row - 1) * size + col + 1);
+        if (
+
+                isOpen(row, col - 1))
+            uf.union((row - 1) * size + col, (row - 1) * size + col - 1);
+        if (
+
+                isOpen(row - 1, col))
+            uf.union((row - 1) * size + col, (row - 2) * size + col);
+        if (
+
+                isOpen(row + 1, col))
+            uf.union((row - 1) * size + col, row * size + col);
 
     }
 
 
     // is the site (row, col) open?
     public boolean isOpen(int row, int col) {
-        if (!(row > 0 && row <= open.length && col > 0 && col <= open.length)) {
-            throw new IllegalArgumentException("row, col indices between 1 and n");
+        if (row > 0 && row <= size && col > 0 && col <= size) {
+            return open[row - 1][col - 1];
         }
-        return open[row - 1][col - 1] == 1;
+        throw new IllegalArgumentException("row, col indices between 1 and n");
     }
 
     // is the site (row, col) full?
     public boolean isFull(int row, int col) {
-        if (!(row > 0 && row <= open.length && col > 0 && col <= open.length)) {
-            throw new IllegalArgumentException("row, col indices between 1 and n");
+        if (row > 0 && row <= size && col > 0 && col <= size) {
+            return uf.find(0) == uf.find((row - 1) * size + col);
         }
-        for (int count = 0; count <= open.length; count++) {
-            if (uf.find(ind[0][count]) == uf.find(ind[row - 1][col - 1])) {
-                return true;
-            }
-        }
-        return false;
+        throw new IllegalArgumentException("row, col indices between 1 and n");
     }
 
     // returns the number of open sites
     public int numberOfOpenSites() {
-        int count = 0;
-        int n = open.length;
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                count += open[i][j];
-            }
-        }
-        return count;
+        return nOpen;
     }
 
     // does the system percolate?
     public boolean percolates() {
-        int n = open.length;
-        int upperVirtualNode = n * n;
-        int lowerVirtualNode = n * n + 1;
-
-        for (
-                int i = 0;
-                i <= n - 1; i++) {
-            if (isOpen(1, i + 1)) {
-                uf.union(ind[0][i], upperVirtualNode);
-            }
-            if (isOpen(n, i + 1)) {
-                uf.union(ind[n - 1][i], lowerVirtualNode);
-            }
-        }
-        return uf.find(upperVirtualNode) == uf.find(lowerVirtualNode);
+        return uf.find(0) == uf.find(number + 1);
     }
 
     // test client (optional)
     public static void main(String[] args) {
+        Percolation perc = new Percolation(Integer.parseInt(args[0]));
+        StdOut.println(perc.isFull(1, 1));
+        StdOut.println(perc.isOpen(1, 1));
+        perc.open(1, 1);
+        perc.open(1, 3);
+        StdOut.println(perc.isFull(1, 1));
+        StdOut.println(perc.isOpen(1, 1));
+        StdOut.println(perc.isFull(3, 6));
+        StdOut.println(perc.isOpen(3, 6));
+        perc.open(3, 6);
+        StdOut.println(perc.isFull(3, 6));
+        StdOut.println(perc.isFull(1, 3));
+        StdOut.println(perc.isOpen(3, 6));
 
     }
 }
--- PercolationStats.java
+++ PercolationStats.java
@@ -4,21 +4,23 @@ import edu.princeton.cs.algs4.StdStats;
 
 
 public class PercolationStats {
-    private static double[] prediction;
+    private double[] prediction;
 
     // perform independent trials on an n-by-n grid
     public PercolationStats(int n, int trials) {
-        for (int t = 0; t < trials; t++) {
+        int number = n * n;
+        prediction = new double[trials];
+        for (int tr = 0; tr < trials; tr++) {
             Percolation perc = new Percolation(n);
             while (!perc.percolates()) {
-                int rd = StdRandom.uniform(n * n - 1);
+                int rd = StdRandom.uniform(number - 1);
                 int row = rd / n + 1;
                 int col = rd % n + 1;
                 if (!perc.isOpen(row, col)) {
                     perc.open(row, col);
                 }
             }
-            prediction[t] = (double) perc.numberOfOpenSites() / (n * n);
+            prediction[tr] = (double) perc.numberOfOpenSites() / number;
         }
     }
 
@@ -47,15 +49,12 @@ public class PercolationStats {
     public static void main(String[] args) {
         int n = Integer.parseInt(args[0]);
         int t = Integer.parseInt(args[1]);
-        prediction = new double[t];
         PercolationStats percStats = new PercolationStats(n, t);
 
-        StdOut.println(percStats.mean());
-        StdOut.println(percStats.stddev());
-        StdOut.println(percStats.confidenceLo());
-        StdOut.println(percStats.confidenceHi());
-
-
+        StdOut.println("mean                    = " + percStats.mean());
+        StdOut.println("stddev                  = " + percStats.stddev());
+        StdOut.println("95% confidence interval = [" + percStats.confidenceLo() + ", " +
+                               percStats.confidenceHi() + "]");
     }
 
 }
